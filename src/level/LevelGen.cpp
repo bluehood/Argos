4b9a2ef230d463583d62f98ef63cd7d70eba70ee
Raphael Isemann <teemperor@gmail.com>
2017-10-30 22:33:54 +0100

    Better water generation based on perlin

--- src/level/LevelGen.cpp
+++ src/level/LevelGen.cpp
@@ -1,17 +1,23 @@
 #include "LevelGen.h"
 
+#include "stb_perlin.h"
 
 Level *LevelGen::generate(GameData &data, int w, int h) {
   this->data = &data;
   level = new Level(w, h, data);
 
+
   for (size_t x = 0; x < w; ++x) {
     for (size_t y = 0; y < h; ++y) {
-      level->get(x, y).setData(data.tile("grass"));
+      float f = stb_perlin_noise3(3 * x / (float) w, 3 * y / (float) h, 7);
+      std::cout << f << std::endl;
+      if (f > -0.2f)
+        level->get(x, y).setData(data.tile("grass"));
+      else
+        level->get(x, y).setData(data.tile("water_c"));
     }
   }
   level->getBuilding(5, 5).setData(data.tile("fireplace"));
-  level->get(8, 8).setData(data.tile("water_c"));
   //level->finalize();
 
   return level;
