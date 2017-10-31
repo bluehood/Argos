ade5a4add2bd3452826f684ae0f95f05dea759b0
Raphael Isemann <teemperor@gmail.com>
2017-10-31 17:21:25 +0100

    Split up vegetation/level generation

--- src/level/LevelGen.cpp
+++ src/level/LevelGen.cpp
@@ -34,11 +34,22 @@ Level *LevelGen::generate(GameData &data, int w, int h) {
 
   for (int x = 0; x < w; ++x) {
     for (int y = 0; y < h; ++y) {
-      float vegetation = stb_perlin_noise3(3 * x / (float) w, 42, 3 * y / (float) h);
       float height = stb_perlin_noise3(3 * x / (float) w, 3 * y / (float) h, 7);
       height += 0.2f;
       if (height > 0) {
         level->get(x, y).setData(data.tile("grass"));
+      } else {
+        level->get(x, y).setData(data.tile("water_c"));
+      }
+    }
+  }
+
+  for (int x = 0; x < w; ++x) {
+    for (int y = 0; y < h; ++y) {
+      float vegetation = stb_perlin_noise3(3 * x / (float) w, 42, 3 * y / (float) h);
+      float height = stb_perlin_noise3(3 * x / (float) w, 3 * y / (float) h, 7);
+      height += 0.2f;
+      if (height > 0) {
         if(vegetation > 0.3f && dis(gen) > 0.7f) {
           make_tree(x, y);
         } else if(vegetation > 0 && dis(gen) > 0.8f) {
@@ -47,7 +58,6 @@ Level *LevelGen::generate(GameData &data, int w, int h) {
           make_bush(x, y, dis(gen));
         }
       } else {
-        level->get(x, y).setData(data.tile("water_c"));
         if(height > -0.1f && vegetation > 0 && dis(gen) > 0.8f) {
           level->getBuilding(x, y).setData(data.tile("lilypad"));
         }
