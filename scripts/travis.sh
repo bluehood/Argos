c97e29a05416d535ea5425d20b6856a34593c723
Raphael Isemann <teemperor@gmail.com>
2017-11-01 16:47:12 +0100

    Added travis support

---
+++ scripts/travis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Go to source dir
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+cd ..
+
+# Create a build folder
+rm -rf travis-build
+mkdir travis-build
+cd travis-build
+cmake ..
+make -j2
